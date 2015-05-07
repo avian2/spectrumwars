b82e6f2d2cbaedc53a3f97109f4e41a486f9780c
Tomaz Solc <tomaz.solc@tablix.org>
2015-05-07 13:15:28 +0200

    Add basic rendezvous player example.

---
+++ examples/rendezvous/player1.py
@@ -0,0 +1,22 @@
+from spectrumwars import Transceiver
+import random
+
+class Transmitter(Transceiver):
+	def start(self):
+		ch = random.randint(0, 40)
+		self.set_configuration(ch, 3, 0)
+
+		while True:
+			self.send()
+
+class Receiver(Transceiver):
+	def start(self):
+		ch = 0
+
+		while True:
+			self.set_configuration(ch, 3, 0)
+
+			for data in self.recv_loop(timeout=.5):
+				pass
+
+			ch = (ch + 1) % 40
