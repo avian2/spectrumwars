f5617f8ef29656bc3ae278d71329aba7248648ff
Tomaz Solc <tomaz.solc@tablix.org>
2015-05-08 13:28:47 +0200

    Add legend

--- controller/spectrumwars/plotter.py
+++ controller/spectrumwars/plotter.py
@@ -4,6 +4,12 @@ import argparse
 from matplotlib import pyplot
 import numpy as np
 
+class ParsedLog:
+	def __init__(self):
+		self.rx = []
+		self.tx = []
+		self.config = []
+
 def plot(args):
 	log = pickle.load(open(args.log_path[0], "rb"))
 
@@ -15,57 +21,74 @@ def plot(args):
 
 	cur_ch = {}
 
+	parsed = { 'rx': ParsedLog(), 'tx': ParsedLog() }
+
 	for event in log:
-		
-		def pl(ch, marker):
-			if 'rx' == event.data['role']:
-				color = (1,0,0)
-			else:
-				color = (0,1,0)
-			
-			if marker=='o':
-				alpha = .5
-				size=10
-				edge=tuple(((np.array(color)+1)/2.))
-			else:
-				alpha = 1.
-				size=7
-				edge=color
-		
-			ax.plot([ch], [event.timestamp-min_t], alpha=alpha, marker=marker, color=color, markersize=size, markeredgecolor=edge, markeredgewidth=1)
-		
+		p = parsed[event.data['role']]
+
 		if event.type == "config":
-			pl(marker='+', ch=event.data['frequency'])
-			cur_ch[event.data['role']] = event.data['frequency']
-		elif event.type == "send":
-			pl(marker='x', ch=cur_ch.get(event.data['role'], 0))
-		elif event.type == "recv":
-			pl(marker='o', ch=cur_ch.get(event.data['role'], 0))
+			l = p.config
+			ch = cur_ch[event.data['role']] = event.data['frequency']
+		else:
+			ch = cur_ch.get(event.data['role'], 0)
+			if event.type == 'send':
+				l = p.tx
+			elif event.type == "recv":
+				l = p.rx
+			else:
+				l = None
+
+		if l is not None:
+			l.append((ch, event.timestamp-min_t))
 		elif event.type == "status":
 			s = np.array(event.data['status'].spectrum)
-			#s -= min(s)
-			#if max(s) > min(s):
-			#	s /= (max(s)-min(s))
-			
+
 			if 'rx' in event.data['role']:
 				cmap = 'YlOrRd_r'
 			else:
 				cmap='YlGn_r'
-				
-			ax.scatter(range(len(s)), 
-					   [event.timestamp - min_t]*len(s), 
-					   c=s, 
-					   s=50, 
-					   marker='s', 
+
+			ax.scatter(range(len(s)),
+					   [event.timestamp - min_t]*len(s),
+					   c=s,
+					   s=50,
+					   marker='s',
 					   linewidths=0,
 					   cmap=cmap,
 					   vmin=-70,
 					   vmax=-10)
-			
+
+	width = 1
+
+	def plot(l, **kwargs):
+		la = np.array(l)
+		if len(la.shape) == 2:
+			ax.plot(la[:,0], la[:,1], ls='none', **kwargs)
+
+	txcolor = (0,1,0)
+	plot(parsed['tx'].rx, label='tx recv', color=txcolor, alpha=.5,
+			marker='o', markersize=10, markeredgecolor=(.5,.1,.5), markeredgewidth=width)
+	plot(parsed['tx'].tx, label='tx send', color=txcolor,
+			marker='x', markeredgewidth=width)
+	plot(parsed['tx'].config, label='tx config', color=txcolor,
+			marker='+', markeredgewidth=width)
+
+	rxcolor = (1,0,0)
+	plot(parsed['rx'].rx, label='rx recv', color=rxcolor, alpha=.5,
+			marker='o', markersize=10, markeredgecolor=(1.,.5,.5), markeredgewidth=width)
+	plot(parsed['rx'].tx, label='rx send', color=rxcolor,
+			marker='x', markeredgewidth=width)
+	plot(parsed['rx'].config, label='rx config', color=rxcolor,
+			marker='+', markeredgewidth=width)
+
+
+
 	ax.set_ylim(0, max_t-min_t)
 	ax.set_xlim(0, 64)
-	pyplot.xlabel("channel")
+	pyplot.xlabel("frequency [channel]")
 	pyplot.ylabel("time [s]")
+
+	pyplot.legend()
 
 	pyplot.grid(color='w')
 
