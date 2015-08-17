f89ae4a059ee666265c1d006206a24263d2c25e5
Tomaz Solc <tomaz.solc@tablix.org>
2015-08-17 12:36:28 +0200

    Make simulated testbed more configurable.

--- controller/spectrumwars/testbed/simulation.py
+++ controller/spectrumwars/testbed/simulation.py
@@ -10,12 +10,14 @@ log = logging.getLogger(__name__)
 class Radio(RadioBase):
 	RECEIVE_TIMEOUT = 2.
 
-	def __init__(self, addr, dispatcher):
+	def __init__(self, addr, dispatcher, send_delay):
 		self.addr = addr
 		self.neighbor = None
 		self.dispatcher = dispatcher
 		self.q = Queue.Queue()
 		self.settings = (0, 0, 0)
+
+		self.send_delay = send_delay
 
 	def _recv(self, addr, data, settings):
 		if self.settings == settings and self.addr == addr:
@@ -26,7 +28,7 @@ class Radio(RadioBase):
 
 	def send(self, data):
 		self.dispatcher(self.neighbor, data, self.settings)
-		time.sleep(.1)
+		time.sleep(self.send_delay)
 
 	def recv(self, timeout=None):
 		if timeout is None:
@@ -41,13 +43,19 @@ class Radio(RadioBase):
 
 class Testbed(TestbedBase):
 
-	def __init__(self):
+	def __init__(self, send_delay=.1, frequency_range=64, bandwidth_range=10, power_range=10, packet_size=1024):
+		self.send_delay = float(send_delay)
+		self.frequency_range = int(frequency_range)
+		self.bandwidth_range = int(bandwidth_range)
+		self.power_range = int(power_range)
+		self.packet_size = int(packet_size)
+
 		self.radios = []
-		self.channels = [0] * self.get_frequency_range()
+		self.channels = [0] * self.frequency_range
 		self.i = 0
 
 	def _get_radio(self):
-		r = Radio(self.i, self._dispatcher)
+		r = Radio(self.i, self._dispatcher, self.send_delay)
 		self.radios.append(r)
 
 		self.i += 1
@@ -85,13 +93,13 @@ class Testbed(TestbedBase):
 		return tuple(spectrum)
 
 	def get_frequency_range(self):
-		return 64
+		return self.frequency_range
 
 	def get_bandwidth_range(self):
-		return 10
+		return self.bandwidth_range
 
 	def get_power_range(self):
-		return 10
+		return self.power_range
 
 	def get_packet_size(self):
-		return 1024
+		return self.packet_size
