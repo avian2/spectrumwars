59844e4588bcb5ed0430cd187029bcdcf02a5b9f
Tomaz Solc <tomaz.solc@tablix.org>
2015-08-17 13:21:56 +0200

    Set version to 0.0.4

--- controller/setup.py
+++ controller/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='spectrumwars',
-    version='0.0.3',
+    version='0.0.4',
     description='A programming game where players compete for radio bandwidth.',
     license='GPL3',
     author='Tomaz Solc',
