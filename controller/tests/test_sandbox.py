074920a32d73cf8f8464b175c9db6dd274319bd0
Tomaz Solc <tomaz.solc@tablix.org>
2015-10-12 15:34:28 +0200

    Add copyright blurbs to all source files.

--- controller/tests/test_sandbox.py
+++ controller/tests/test_sandbox.py
@@ -1,3 +1,23 @@
+# Copyright (C) 2015 SensorLab, Jozef Stefan Institute http://sensorlab.ijs.si
+#
+# Written by Tomaz Solc, tomaz.solc@ijs.si
+#
+# This work has been partially funded by the European Community through the
+# 7th Framework Programme project CREW (FP7-ICT-2009-258301).
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see http://www.gnu.org/licenses/
+
 import imp
 import logging
 import os
